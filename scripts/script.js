9b7d2cdf729785ccbf38c8dc936c2e1b7efdd26f
LinseyQuat <94555176+LinseyQuat@users.noreply.github.com>
2022-01-24 21:14:52 +0100

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,22 @@
-// JavaScript Document
+var buttonGroter = document.querySelector(".knopje")
+var body = document.querySelector("body")
+
+
+
+buttonGroter.addEventListener("click", groter)
+  
+function groter () {
+      body.classList.toggle("groter")
+  }
+
+
+  
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+  }
+  
+  function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+  }
+
+
